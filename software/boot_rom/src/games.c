142cf1304c66da514ba2da097278454ed20a2816
Guillaume <guillaume.beguin@gmail.com>
2025-05-15 18:50:42 +0200

    Improve boot rom

--- software/boot_rom/src/games.c
+++ software/boot_rom/src/games.c
@@ -39,19 +39,33 @@ static void displayGamePage() {
     u16 startIndex = page * GAMES_PER_PAGE;
     u16 lastIndex = menu_gamecount - 1;
     u16 pageLastIndex = (page +1) * GAMES_PER_PAGE - 1;
-    u16 endIndex = pageLastIndex;
-    if (pageLastIndex > lastIndex) {
-        endIndex = lastIndex;
-    }
+    u16 endIndex = (pageLastIndex > lastIndex) ? lastIndex : pageLastIndex;
+    u16 pageCount = lastIndex / GAMES_PER_PAGE + 1;
+
+    VDP_setTextPalette(2);
 
     char buffer[32];
     sprintf(buffer, "%d games found", menu_gamecount);
-    VDP_setTextPalette(2);
     VDP_drawText(buffer, 1, 1);
 
+    sprintf(buffer, "Page %d on %d", page + 1, pageCount);
+    VDP_drawText(buffer, 1, 2);
 
     for(u16 i = startIndex; i <= endIndex; i++) {
         printGameName(i);
+    }
+
+    u8 lastY = 4 + 2 * (endIndex % GAMES_PER_PAGE);
+    u8 nextY = lastY + 2;
+
+    u8 nextX = (page == 0) ? 1 : 13;
+
+    VDP_setTextPalette(2);
+    if (page > 0) {
+        VDP_drawText("< Previous", 1, nextY);
+    }
+    if (page < pageCount - 1) {
+        VDP_drawText("Next >", nextX, nextY);
     }
 }
 
@@ -62,5 +76,5 @@ static void printGameName(u16 position) {
     memcpy(name, (const char *)(menu_gamelist + position * GAME_ENTRY_SIZE), GAME_ENTRY_SIZE);
     name[GAME_ENTRY_SIZE] = 0; // null-terminate
 
-    VDP_drawText(name, 2, 5 + 2 * (position % GAMES_PER_PAGE));
+    VDP_drawText(name, 2, 4 + 2 * (position % GAMES_PER_PAGE));
 }
