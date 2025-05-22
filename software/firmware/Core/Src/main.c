82531786107028e80de34c93aefa401f34ef5a1e
Guillaume <guillaume.beguin@gmail.com>
2025-05-22 11:21:09 +0200

    [Firm] Enable FatFs

--- software/firmware/Core/Src/main.c
+++ software/firmware/Core/Src/main.c
@@ -18,6 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "fatfs.h"
 #include "spi.h"
 #include "gpio.h"
 
@@ -88,6 +89,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_SPI1_Init();
+  MX_FATFS_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
